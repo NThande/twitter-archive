c43703b74975eb0d485d98dfa0f3564850ef3ef9
nthande <nthande@outlook.com>
2017-07-17 12:06:35 -0400

    Tweets now go to db

--- sharkweek.py
+++ sharkweek.py
@@ -9,9 +9,12 @@ def get_creds():
     return data
 
 # Takes in oAuth keys and returns tweets containing #sharkweek as .json.
-def get_tweets(consumer_key, consumer_secret):
-    api = TwitterAPI(consumer_key, consumer_secret, auth_type='oAuth2')
-    r = api.request('search/tweets', {'q': '#sharkweek'})
+def get_tweets(hash, count):
+    creds = get_creds()
+    key = creds['Consumer Key']
+    secret = creds['Consumer Secret']
+    api = TwitterAPI(key, secret, auth_type='oAuth2')
+    r = api.request('search/tweets', {'q': hash, 'count': count})
     return r
 
 # Connects to db_file and returns connected variable.
@@ -23,37 +26,61 @@ def create_connection(db_file):
         print(e)
     return None
 
-# Get tweets and store in results
-creds = get_creds()
-key = creds['Consumer Key']
-secret = creds['Consumer Secret']
+def create_cursor(connect):
+    try:
+        c = connect.cursor()
+        return c;
+    except sqlite3.Error as e:
+        print(e)
+    return None
 
-results = get_tweets(key, secret)
-r = results.json()
-# Print tweets for viewing
+#def auth():
+
+# Authenticate to Twitter
+results = get_tweets('#sharkweek', 100)
+
+# r = results.json()
 # print(type(r))
 # print(type((r,)))
-# for item in results.get_iterator():
-#     print(item['user']['screen_name'], item['text'], item['id'])
 
-with open('tweets.json', 'w') as outfile:
-      json.dump(r, outfile)
+# Connect to db
+my_db = "sharkweek.db"
+conn = create_connection(my_db)
+c = create_cursor(conn)
 
-# Print types and data entries to find fields for SQLite
-# print(results.json())
-print(r.keys())
-rStat = r['statuses']
-rMeta = r['search_metadata']
-rEnt = rStat[-1]
-print("Statuses are of type:", type(rStat))
-print("Entries in Status:", rStat[0:-1])
-print("Entries are of type:", type(rStat[-1]))
-print("Tags in Entries:", rEnt.keys())
-print("Tags are of type:", type(rEnt['user']))
-# print("Metadata is type:", type(rMeta))
-# print("Keys in Metadata:", rMeta.keys())
+# Retrieve metadata for each tweet and add to db
+for item in results.get_iterator():
+    print(item['user']['screen_name'], item['text'], item['id'])
+    # id = item['id']
+    # user = item['user']['name']
+    # screen_name = item['user']['screen_name']
+    # text = item['text']
+    # id_str = item['id_str']
+    # retweeted = item['retweeted']
+    # retweet_count = item['retweet_count']
+    # favorited = item['favorited']
+    # favorite_count = item['favorite_count']
+    # if 'possibly_sensitive' in item:
+    #     possibly_sensitive = item['possibly_sensitive']
+    # else:
+    #     possibly_sensitive = ''
+    # lang = item['lang']
+    # source = item['source']
+    # created_at = item['created_at']
+    #
+    # # Add metadata to db
+    # try:
+    #     c.execute("INSERT into twitter VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    #               [id, user, screen_name, text, id_str, retweeted, retweet_count, favorited,
+    #                favorite_count, possibly_sensitive, lang, source, created_at])
+    # except sqlite3.Error as e:
+    #     print(e)
 
-#Connect to db
+# Savve the changes
+conn.commit()
+conn.close()
+
+# Connect to db
 # my_db = "sharkweek.db"
 # conn = create_connection(my_db)
 #
@@ -65,3 +92,29 @@ print("Tags are of type:", type(rEnt['user']))
 # finally:
 #     conn.commit()
 #     conn.close()
+
+#c.execute("INSERT into twitter VALUES(?,?,?,?)", ['','','',r])
+
+# with open('tweets.json', 'w') as outfile:
+#       json.dump(r, outfile)
+
+# Print types and data entries to find fields for SQLite
+# print(results.json())
+# print(r.keys())
+# rLay1 = r['statuses']
+# rMeta = r['search_metadata']
+# rLay2 = rLay1[-1]
+# rLay3 = rLay2['user']
+# rLay4 = rLay3['screen_name']
+# print("Layer 1 is type:", type(rLay1))
+# print("Layer 2 entries in Layer 1:", rLay1[0:-1])
+# print("Layer 2 is type:", type(rLay2))
+# print("Layer 3 entries in Layer 2:", rLay2.keys())
+# print("Layer 3 is type:", type(rLay3))
+# print("Layer 4 entries in Layer 3:", rLay3.keys())
+# print("Layer 4 is type: ", type(rLay4))
+# print("Layer 4 is the end layer")
+# print("Metadata is type:", type(rMeta))
+# print("Keys in Metadata:", rMeta.keys())
+
+
--- sqlite.py
+++ sqlite.py
@@ -23,7 +23,16 @@ sql_create_twitter_table = '''CREATE TABLE IF NOT EXISTS twitter (
                               id integer PRIMARY KEY,
                               user text,
                               screen_name text,
-                              text BLOB);
+                              text text,
+                              id_str text,
+                              retweeted numeric,
+                              retweet_count integer,
+                              favorited numeric,
+                              favorite_count integer,
+                              possibly_sensitive numeric,
+                              lang text,
+                              source text,
+                              created_at text);
                            '''
 my_db = "sharkweek.db"
 conn = create_connection(my_db)
