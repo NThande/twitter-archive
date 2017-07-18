86916cd0228bc4500614b723994ebf78f31e4959
nthande <nthande@outlook.com>
2017-07-18 12:01:05 -0400

    Removed non-API files

--- droptable.py
+++
@@ -1,8 +0,0 @@
-# Simple script to drop the current db table.
-
-import twitterSQLite
-import variables
-
-conn = twitterSQLite.create_connection(variables.db_file)
-twitterSQLite.drop_table(conn, variables.table_name)
-twitterSQLite.close_connection(conn)
