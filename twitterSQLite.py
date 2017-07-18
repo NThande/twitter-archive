799d751c93a1fa9b42ebe4ee9c9b708a01b1228a
nthande <nthande@outlook.com>
2017-07-18 09:58:15 -0400

    Removed duplicate functions from twitterSQLite.py

--- twitterSQLite.py
+++ twitterSQLite.py
@@ -1,8 +1,9 @@
 from sqlite3 import Error
 from sqlite3 import connect
 from TwitterAPI import TwitterAPI
+import json
 
-# Connects to an SQLite DB file.
+# Connects to an SQLite DB file and returns connected variable
 def create_connection(db_file):
     try:
         conn = connect(db_file)
@@ -11,12 +12,22 @@ def create_connection(db_file):
         print(e)
     return None
 
+# Saves changes to db connected by conn and closes conn.
 def close_connection(conn):
     try:
         conn.commit()
         conn.close()
     except Error as e:
         print(e)
+
+# Creates cursor object from connection object.
+def create_cursor(conn):
+    try:
+        c = conn.cursor()
+        return c;
+    except Error as e:
+        print(e)
+    return None
 
 # Creates table with name table_name and columns in col_dict in database connected by conn. col_dict must contain
 # col_name: col_type format.
@@ -32,7 +43,7 @@ def create_table(conn, table_name, col_dict):
 
     # Execute table creation
     try:
-        c = conn.cursor()
+        c = create_cursor(conn)
         c.execute(table_string)
     except Error as e:
         print(e)
@@ -40,7 +51,7 @@ def create_table(conn, table_name, col_dict):
 # Deletes table with table_name from database connected by conn.
 def drop_table(conn, table_name):
     try:
-        c = conn.cursor()
+        c = create_cursor(conn)
         c.execute("DROP TABLE {}".format(table_name))
     except Error as e:
         print(e)
@@ -48,7 +59,7 @@ def drop_table(conn, table_name):
 # Adds columns from col_dict to table with table_name in database connected by conn.
 def alter_table(conn, table_name, col_dict):
     try:
-        c = conn.cursor()
+        c = create_cursor(conn)
         for item in col_dict:
             try:
                 c.execute("ALTER TABLE {} ADD COLUMN {} {}".format(table_name, item, col_dict[item]))
@@ -57,8 +68,8 @@ def alter_table(conn, table_name, col_dict):
     except Error as e:
         print(e)
 
-# Takes in oAuth keys from config.json and returns n tweets containing hash as an API response.
-def get_tweets(hash, n):
+# Takes in oAuth keys from config.json and returns tweet_count tweets containing hash as an API response.
+def get_tweets(hash, tweet_count):
 
     # Retrieve keys from config file
     with open('config.json') as json_data_file:
@@ -68,36 +79,18 @@ def get_tweets(hash, n):
 
     # Get tweets from Twitter
     api = TwitterAPI(key, secret, auth_type='oAuth2')
-    r = api.request('search/tweets', {'q': hash, 'count': n})
+    r = api.request('search/tweets', {'q': hash, 'count': tweet_count})
     return r
 
-# Connects to db_file and returns connected variable.
-def create_connection(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except sqlite3.Error as e:
-        print(e)
-    return None
+# Updates table table_name in db connected by conn with tweet info from TwitterAPI response tweet_response. col_dict
+# determines which columns of metadata are added to the db.
+def update_database(tweet_response, col_dict, conn, table_name):
 
-# Creates cursor object from connection object.
-def create_cursor(conn):
-    try:
-        c = conn.cursor()
-        return c;
-    except sqlite3.Error as e:
-        print(e)
-    return None
+    col_list = list(col_dict.keys())
+    cursor = create_cursor(conn)
+    count = len(col_list)
 
-# Closes the connection object.
-def close_conn(conn):
-    conn.commit()
-    conn.close()
-    return None
-
-def update_database(response, meta, cursor, table, count):
-
-    # Create the placeholders for each column
+    # Create the "?" placeholders for each column
     if count <= 0:
         return None
     col_string = ""
@@ -107,10 +100,10 @@ def update_database(response, meta, cursor, table, count):
     col_string += "?"
 
     # Extract metadata from tweets
-    for tweet in response.get_iterator():
+    for tweet in tweet_response.get_iterator():
         tweet_data = []
         for i in range(count):
-            entry = meta[i]
+            entry = col_list[i]
             if entry in tweet:
                 tweet_data.append(tweet[entry])
             else:
@@ -119,7 +112,7 @@ def update_database(response, meta, cursor, table, count):
 
         # Add metadata to db
         try:
-            cursor.execute("INSERT into {} VALUES({})".format(table, col_string),
+            cursor.execute("INSERT into {} VALUES({})".format(table_name, col_string),
                            tweet_data)
-        except sqlite3.Error as e:
+        except Error as e:
             print(e)
