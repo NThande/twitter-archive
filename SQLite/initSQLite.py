659fc16bc492c70c6e88ef3e732c4ffccd51b7ef
nthande <nthande@outlook.com>
2017-07-31 09:10:01 -0400

    Cleanup of initSQLite.py and initMSSQL.py

--- SQLite/initSQLite.py
+++ SQLite/initSQLite.py
@@ -5,7 +5,7 @@ from SQLite import variables
 
 conn = twitter_SQLite.create_connection(variables.db_file)
 cur = twitter_SQLite.create_cursor(conn)
-twitter_SQLite.create_table(cur, variables.table_name, variables.col_dict)
+# twitter_SQLite.create_table(cur, variables.table_name, variables.col_dict)
 # twitter_SQLite.alter_table(cur, variables.table_name, variables.col_dict)
 # twitter_SQLite.drop_table(cur, variables.table_name)
 twitter_SQLite.close_connection(conn)
